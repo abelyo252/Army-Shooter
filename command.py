6f8371f95d2a34d5f7dffa2d5ffa7f9483d81aa0
Abel Yohannes <126100289+abelyo252@users.noreply.github.com>
2023-05-31 17:58:59 -0400

    Update command.py

--- command.py
+++ command.py
@@ -1,3 +1,7 @@
+import threading
+import main
+
+
 
 def boundaryChecker(img, shot , OFFSET):
 
@@ -25,53 +29,87 @@ def mkDecision(img, shot,OFFSET):
 
     else:
 
+        # Quandrant 2
         if x < (w // 2) and y < (h // 2):
             cmd1, cmd2 = -1, 1
             return cmd1, cmd2
+
+        # Quandrant 1
         elif x > (w // 2) and y < (h // 2):
             cmd1, cmd2 = 1, 1
             return cmd1, cmd2
+
+        # Quadrant 3
         elif x < (w // 2) and y > (h // 2):
             cmd1, cmd2 = -1, -1
             return cmd1, cmd2
+
+        # Quadrant 4
         elif x > (w // 2) and y > (h // 2):
             cmd1, cmd2 = 1, -1
             return cmd1, cmd2
 
 def cmdInterpret(cmd1, cmd2):
     if cmd1 == -1 and cmd2 == 1:
-        return "RIGHT , DOWN"
-    elif cmd1 == 1 and cmd2 == 1:
-        return "LEFT , DOWN"
-    elif cmd1 == -1 and cmd2 == -1:
         return "RIGHT , UP"
-    elif cmd1 == 1 and cmd2 == -1:
+    elif cmd1 == 1 and cmd2 == 1:
         return "LEFT , UP"
+    elif cmd1 == -1 and cmd2 == -1:
+        return "RIGHT , DOWN"
+    elif cmd1 == 1 and cmd2 == -1:
+        return "LEFT , DOWN"
     elif cmd1 == 0 and cmd2 == 0:
         return "AC"
 
 def sendMessage(recv_message, ser):
-    if recv_message != "AC" or recv_message != "RR":
-        recv_message = recv_message.split()
-        message = recv_message[0][0] + recv_message[1][1]
+
+    # Acquired Signal
+    if recv_message == "AC":
+        message = "5"
         try:
             ser.write(message.encode('utf-8'))
         except Exception as e:
             print("Unable to send data", e)
 
+    # Rotate Signal
     elif recv_message == "RR":
-        message = "RR"
-        try:
-            ser.write(message.encode('utf-8'))
-        except Exception as e:
-            print("Unable to send data", e)
-    else:
-        message = "AC"
+        message = "6"
         try:
             ser.write(message.encode('utf-8'))
         except Exception as e:
             print("Unable to send data", e)
 
+    # Left UP Signal
+    elif recv_message == "LEFT , UP":
+        message = "23"
+        try:
+            ser.write(message.encode('utf-8'))
+        except Exception as e:
+            print("Unable to send data", e)
+
+    # Left Down Signal
+    elif recv_message == "LEFT , DOWN":
+        message = "24"
+        try:
+            ser.write(message.encode('utf-8'))
+        except Exception as e:
+            print("Unable to send data", e)
+
+    # Right Up Signal
+    elif recv_message == "RIGHT , UP":
+        message = "13"
+        try:
+            ser.write(message.encode('utf-8'))
+        except Exception as e:
+            print("Unable to send data", e)
+
+    # Right Down Signal
+    elif recv_message == "RIGHT , DOWN":
+        message = "14"
+        try:
+            ser.write(message.encode('utf-8'))
+        except Exception as e:
+            print("Unable to send data", e)
 
 
 def left_manual(ser):
@@ -102,12 +140,12 @@ def down_manual(ser):
     except Exception as e:
         print("Unable to send data", e)
 
-def receive_data(ser,q):
+def receive_data(ser):
+    data = None
     while True:
         try:
             data = ser.readline().decode().strip()
-            q.put(data)
-            print("Data Received : ",data)
-
+            if data!= None:
+                event.set()
         except:
             pass
